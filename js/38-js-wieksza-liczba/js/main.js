da85657a8b5de8fbfb60b689ec23cf78866d5e4b
Adam Klin <adam.klin@assecobs.pl>
2020-05-20 19:18:01 +0200

    ex. 38

---
+++ js/38-js-wieksza-liczba/js/main.js
@@ -0,0 +1,10 @@
+var num1 = 2;
+var num2 = 5;
+
+function compare(first, second) {
+  if (first > second) return first;
+  if (second > first) return second;
+  return "liczby są takie same";
+}
+
+console.log(compare(num1, num2));
