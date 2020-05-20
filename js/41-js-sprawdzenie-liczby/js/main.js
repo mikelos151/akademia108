90a2d2b8772db602c1699c76efb8197debab0669
Adam Klin <adam.klin@assecobs.pl>
2020-05-20 19:32:43 +0200

    ex. 41'

---
+++ js/41-js-sprawdzenie-liczby/js/main.js
@@ -0,0 +1,6 @@
+function range(n){
+  if (n >=100 && n<=200) return console.log("Liczba znajduje się w przedziale");
+   else return console.log("Liczba nie znajduje się w przedziale");
+}
+
+range(99)
