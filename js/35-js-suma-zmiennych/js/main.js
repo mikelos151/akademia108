0d29307dd9d335c85b24ba86115ebf2d2ae2716d
Adam Klin <adam.klin@assecobs.pl>
2020-05-20 11:31:12 +0200

    ex. 35

---
+++ js/35-js-suma-zmiennych/js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 5;
+let secondNumber = 8;
+
+function suma(firstNumber, secondNumber) {
+  let suma = firstNumber + secondNumber;
+  return suma;
+}
+
+//console.log(suma(firstNumber, secondNumber));
