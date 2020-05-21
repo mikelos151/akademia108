cdeb9fe6a653d985e38aeb74fb2891a520e05504
Adam Klin <adam.klin@assecobs.pl>
2020-05-21 12:43:27 +0200

    ex.41 dodano nawiasy klamrowe

--- js/41-js-sprawdzenie-liczby/js/main.js
+++ js/41-js-sprawdzenie-liczby/js/main.js
@@ -1,6 +1,10 @@
 function range(n){
-  if (n >=100 && n<=200) return console.log("Liczba znajduje się w przedziale");
-   else return console.log("Liczba nie znajduje się w przedziale");
+  if (n >=100 && n<=200) {
+    return console.log("Liczba znajduje się w przedziale");
+  }
+   else {
+     return console.log("Liczba nie znajduje się w przedziale");
+    }
 }
 
 range(99)
