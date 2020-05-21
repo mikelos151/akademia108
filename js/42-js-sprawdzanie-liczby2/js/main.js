0a50b4bc05d3fa78f32695f3e1d4fe6672dadcde
Adam Klin <adam.klin@assecobs.pl>
2020-05-21 12:47:08 +0200

    ex. 38 formatowanie

--- js/42-js-sprawdzanie-liczby2/js/main.js
+++ js/42-js-sprawdzanie-liczby2/js/main.js
@@ -1,9 +1,9 @@
 function checkNumbers(num1,num2) {
   let sum =parseInt(num1)+ parseInt(num2);
-if (num1===20 || num2===20 || sum<=20 ) {
-  return true;
-}
-  return false
+  if (num1===20 || num2===20 || sum<=20 ) {
+    return true;
+  }
+    return false
 }
 
 console.log(checkNumbers(18,3));
