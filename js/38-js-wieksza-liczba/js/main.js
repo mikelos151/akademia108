8a5e91878ce1cfa4df70f4bb02d96e0a5d3b8436
Adam Klin <adam.klin@assecobs.pl>
2020-05-21 12:33:01 +0200

    ex. 38 dodano nawiasy klamrowe

--- js/38-js-wieksza-liczba/js/main.js
+++ js/38-js-wieksza-liczba/js/main.js
@@ -2,8 +2,8 @@ var num1 = 2;
 var num2 = 5;
 
 function compare(first, second) {
-  if (first > second) return first;
-  if (second > first) return second;
+  if (first > second) {return first};
+  if (second > first) {return second};
   return "liczby są takie same";
 }
 
