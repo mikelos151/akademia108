8b470ac08afec9720cb38853920914653c0530a6
adam.klin <adamklin86@gmail.com>
2020-06-07 11:29:03 +0200

    fibonaci

--- js/5-element-fibonacci/js/main.js
+++ js/5-element-fibonacci/js/main.js
@@ -2,16 +2,13 @@
 // Jako parametr funkcja przyjmuje n - czyli n-ty element ciągu fibbonaciego.
 
 const fibonaci = (n) => {
-  let array = [0, 1];
-  for (let x = 2; x <= n; x++) {
-    array.push(array[x - 2] + array[x - 1]);
+  let array = [];
+  for (let x = 0; x <= n; x++) {
+    (x < 2) ? array.push(x): array.push(array[x - 2] + array[x - 1]);
   }
   return array[n]
 }
 
-console.log(fibonaci(50));
-
-
-//354224848179261915075
+console.log(fibonaci(13));
 
 // 0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	610	987	1597	2584	4181
