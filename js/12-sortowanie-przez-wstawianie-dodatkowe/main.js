a52949176228948382fc14cfa24ba1d9005eea73
Adam Klin <adam.klin@assecobs.pl>
2020-06-08 17:22:53 +0200

    js dodatkowe

---
+++ js/12-sortowanie-przez-wstawianie-dodatkowe/main.js
@@ -0,0 +1,7 @@
+// Zaimplementuj funkcję, która sortuje tablicę przez wstawianie.
+// Funkcja przyjmuje parametr - tj. tablicę do posortowania, a zwraca posortowaną talicę.
+// Wywołaj funkcję z tablicą [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67, 98, 1]. Wynik jej działania wyświetl w konsoli.
+// Zadanie domowe robimy z wykorzystaniem serwisu https://repl.it
+// Podpowiedź przed wysłaniem zadania do sprawdzenia:
+// - znajdź w internecie opis działania sortowania przez wstawianie - polecamy filmik pokazujący działania tego algorytmu https://www.youtube.com/watch?v=ROalU379l3U
+// - zastosuj pętlę w pętli :)
