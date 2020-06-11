a7700071b94a6223cf2cf7dfa8f06b56835189d2
adam.klin <adamklin86@gmail.com>
2020-06-11 20:22:46 +0200

    ex. JS DOM

---
+++ js/modelDOM/5-walidacja-formularza-zapisu-do-newslettera/main.js
@@ -0,0 +1,46 @@
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+const form = document.querySelector("form");
+const firstLastName = document.querySelector("[name='firstLastName']");
+const email = document.querySelector("[name='email']");
+const errors = [];
+
+const validate = (e) => {
+  e.preventDefault();
+  errors.length = 0; //czyszczenie błędów
+  firstLastName.value.length == 0 ? errors.push("Wpisz imię i nazwisko") : null;
+  email.value.length == 0 ? errors.push("Wpisz e-mail") : null;
+  !email.value.includes("@") ? errors.push("Wpisz poprawny adres e-mail") : null;
+  !checkboxes[1].checked ? errors.push("Zaznacz zgodę marketingową nr. 1") : null;
+
+  //wyświetl błędy
+  console.log(errors);
+
+  if (errors.length > 0) {
+    //czysczenie błędów 
+    document.querySelector("ul") !== null ? document.querySelector("ul").remove() : null;
+    const ulErrorList = document.createElement("ul");
+    document.body.insertBefore(ulErrorList, form);
+
+    for (error of errors) {
+      const liElement = document.createElement("li");
+      const liNodeText = document.createTextNode(error);
+      liElement.appendChild(liNodeText);
+      ulErrorList.appendChild(liElement);
+    }
+  }
+}
+
+const checkUncheck = () => {
+  for (let x = 1; x < checkboxes.length; x++) {
+    if (checkboxes[0].checked) {
+      checkboxes[x].checked = true;
+      checkboxes[x].disabled = true;
+    } else {
+      checkboxes[x].checked = false;
+      checkboxes[x].disabled = false;
+    }
+  }
+}
+
+checkboxes[0].addEventListener("change", checkUncheck);
+form.addEventListener("submit", validate);
