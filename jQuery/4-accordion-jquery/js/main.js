4928d802cf7fc8228b4ad4ad7a0b27583e876dec
Adam Klin <adam.klin@assecobs.pl>
2020-06-18 13:58:04 +0200

    4-accordion-jquery

--- jQuery/4-accordion-jquery/js/main.js
+++ jQuery/4-accordion-jquery/js/main.js
@@ -1,22 +1,36 @@
 $(document).ready(function () {
-  // console.log($('.accordion-content'));
-  let toggle = false;
+
   $('.accordion-item').on('click', function () {
-    if (toggle) {
-      $(this).children().animate({
-          height: '0px'
-        }, 500,
-        function () {
-          toggle = false;
-        })
-    } else if (!toggle) {
-      $(this).children().animate({
-          height: '500px'
-        }, 500,
-        function () {
-          toggle = true;
-        })
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open');
+      $(this).find('.accordion-content').slideUp()
+    } else {
+      $(this).addClass('open');
+      $(this).find('.accordion-content').slideDown()
     }
+
+
 
   })
 })
+
+
+
+// if (toggle) {
+//   $(this).children().animate({
+//       height: '0px',
+//       display: 'none'
+//     }, 500,
+//     function () {
+//       toggle = false;
+//     })
+// } else if (!toggle) {
+//     $(this).children().animate({
+//         height: '500px',
+//         display: ''
+//       }, 500,
+//       function () {
+//         toggle = true;
+//       })
+//   }
+// })
