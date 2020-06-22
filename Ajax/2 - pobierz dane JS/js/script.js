a49c64fdae6cbfc0cbb066b80356d8e52374832c
Adam Klin <adam.klin@assecobs.pl>
2020-06-22 14:55:13 +0200

    pobierz dane czysty js

---
+++ Ajax/2 - pobierz dane JS/js/script.js
@@ -0,0 +1,11 @@
+const getData = () => {
+  fetch('https://akademia108.pl/api/ajax/get-post.php')
+    .then(response => response.json())
+    .then(data => {
+      document.body.appendChild(document.createElement('div'));
+      let div = document.querySelector('div');
+      div.innerHTML = `ID: ${data.id} <br> ${data.userId} <br> ${data.title} <br> ${data.body}`
+    })
+}
+
+document.querySelector('button').addEventListener('click', getData);
